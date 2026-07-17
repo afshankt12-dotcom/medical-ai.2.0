7235e26734ced96b62ac541c857e6cceb33fad8a
unknown <afshankt12@gmail.com>
2026-07-17 23:50:47 +0530

    Load retrieval dataset from Hugging Face

--- src/config.py
+++ src/config.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 
+from huggingface_hub import hf_hub_download
+
+
 # Project Root
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Main folders
 DATASET_DIR = BASE_DIR / "dataset"
 MODELS_DIR = BASE_DIR / "models"
+
 
 # ==========================
 # Dataset Files
@@ -17,9 +22,15 @@ MEDQUAD_HF_PATH = DATASET_DIR / "medquad_hf.parquet"
 
 PUBMEDQA_PATH = DATASET_DIR / "pubmedqa.parquet"
 
-CHATBOT_DATASET_PATH = DATASET_DIR / "medical_chatbot_dataset.csv"
+
+CHATBOT_DATASET_PATH = hf_hub_download(
+    repo_id="shanufewf/medical-transformer-model",
+    filename="medical_chatbot_dataset.csv"
+)
+
 
 ORIGINAL_PQAL_PATH = DATASET_DIR / "ori_pqal.json"
+
 
 # Future datasets
 
